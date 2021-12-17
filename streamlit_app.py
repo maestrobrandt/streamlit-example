4679ad010ca92ad206109166142f87391e6fbcfb
Zachary Brandt <69327122+maestrobrandt@users.noreply.github.com>
2021-12-16 23:12:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,9 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import pickle
 
-df = pd.read_csv('Case - Study Dataset.xlsx - Sheet1.csv')
-
-from sklearn.model_selection import train_test_split, cross_val_score
-features = ['GRE', 'TOPNOTCH', 'GPA']
-
-X = df[features]
-y = df['ADMIT'].values
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
-
-from imblearn.over_sampling import RandomOverSampler
-ros = RandomOverSampler(random_state=0)
-X_train_resampled, y_train_resampled = ros.fit_sample(X_train, y_train)
-
-X_train_resampled_scaled = scaler.transform(X_train_resampled)
-X_test_scaled = scaler.transform(X_test)
-
-from sklearn.ensemble import RandomForestClassifier
-
-forest = RandomForestClassifier(n_estimators = 100, min_samples_split = 5, min_samples_leaf = 8, max_features ='sqrt',
- max_depth = 60)
-
-forest.fit(X_train_resampled_scaled, y_train_resampled)
+forest = pickle.load(open( "forest.p", "rb" ))
 
 st.title('Admission Predictor')
 st.subheader('GPA:')
